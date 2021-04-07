b59d73ab73a64cc59024516a8b6ce8ac3a0a7cba
Ron Booth <ron@ronbooth.com>
2021-04-07 10:33:31 -0600

    license updates

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,6 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: FlexText(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text('$_counter', style: Theme.of(context).textTheme.headline4),
