654f271f1fc6397b3b8ffdb3a3adb3e9eaadb7b2
Ron Booth <ron@ronbooth.com>
2021-06-01 05:46:17 -0600

    License update, and added copyright text to some more source files.

--- lib/src/render_object_ext.dart
+++ lib/src/render_object_ext.dart
@@ -1,3 +1,6 @@
+// Copyright 2021 Ron Booth. All rights reserved.
+// Use of this source code is governed by a license that can be found in the LICENSE file.
+
 import 'package:flutter/rendering.dart';
 
 /// Signature for a function that takes an [Object] and returns `true` to continue,
--- test/util_test.dart
+++ test/util_test.dart
@@ -1,3 +1,6 @@
+// Copyright 2021 Ron Booth. All rights reserved.
+// Use of this source code is governed by a license that can be found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
